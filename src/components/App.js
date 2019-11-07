08a4235bddea943bea7bd5ac541b25a0595758a0
Steve Golden <sgolden2525@outlook.com>
2019-11-07 04:21:42 -0600

    Shelf branch option select
    Added shelf branch option selection functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,14 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
-  super(props)
+    super(props)
+
+    this.shouldReRender = this.shouldReRender.bind(this);
   }
 
   state = {
-    allBooks: []
+    allBooks: [],
+    isReRender: false,
   }
 
   componentDidMount() {
@@ -22,18 +25,26 @@ class App extends Component {
       })
   }
 
+  shouldReRender() {
+    console.log("shouldReRender")
+    this.setState((currentState) => ({
+      isReRender: !this.state.isReRender
+    }))
+
+    console.log(this.state.isReRender)
+  }
+
   render() {
-    console.log(this.state.allBooks)
     return (
       <div className="App">
         <div className="mainpage__mainpagelinkWrapper">
           <Link to='/Search'>Search Book Library</Link>
         </div> 
         <h1 className="center-text">My Book Reads</h1>
-          <header className="App-header">          
-          <BookCategories categoryName='Currently Reading' books={this.state.allBooks.filter((book) => book.shelf === 'currentlyReading')} />
-          <BookCategories categoryName='Want to Read' books={this.state.allBooks.filter((book) => book.shelf === 'wantToRead')} />
-          <BookCategories categoryName='Read' books={this.state.allBooks.filter((book) => book.shelf === 'read')} />
+        <header className="App-header">
+          <BookCategories categoryName='Currently Reading' books={this.state.allBooks.filter((book) => book.shelf === 'currentlyReading')} shouldReRender={this.shouldReRender} />
+          <BookCategories categoryName='Want to Read' books={this.state.allBooks.filter((book) => book.shelf === 'wantToRead')} shouldReRender={this.shouldReRender} />
+          <BookCategories categoryName='Read' books={this.state.allBooks.filter((book) => book.shelf === 'read')} shouldReRender={this.shouldReRender} />
         </header>
         </div>
    
--- src/components/ShelfTransferMenu.js
+++ src/components/ShelfTransferMenu.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
+import * as BooksAPI from '../BooksAPI.js';
 import '../App.css';
 
 class ShelfTransferMenu extends Component {
   constructor(props) {
     super(props)
+
+    this.changeShelf = this.changeShelf.bind(this)
+
+  }
+  
+ 
+ 
+  changeShelf(event) {
+
+    /***** Callback to parent Allbooks to change shelf *****/
+
+
+
+    if (event.target.value !== this.props.isShelf) { 
+      console.log(event.target.value)
+      BooksAPI.update(this.props.book, event.target.value)
+        .then((shelf) => {
+          console.log(shelf)
+
+        })
+      window.location.reload();
+     }
+    
   }
 
- 
-
-
-  render() {
-    return (
+  render() {  
+    let shelf = this.props.isShelf;
+     return (
       < form >
-        <select className="mainpage__select" size="5" visiblity={this.props.isSelectVisible === true ? 'visible' : 'hidden'} >
+         <select className="mainpage__select" size="5" visiblity={this.props.isSelectVisible === true ? 'visible' : 'hidden'} defaultValue={this.props.shelf} onChange={(event) => this.changeShelf(event)} >
           <option disabled>Move to...</option>
-          <option className="mainpage__option" value="0">Currently Reading</option>
-          <option className="mainpage__option" value="1">Want to Read</option>
-          <option className="mainpage__option" value="2">Read</option>
-          <option className="mainpage__option" value="">None</option>
+           <option className="mainpage__option" value="currentlyReading">Currently Reading</option>
+           <option className="mainpage__option" value="wantToRead">Want to Read</option>
+           <option className="mainpage__option" value="read">Read</option>
+          <option className="mainpage__option" value="none">None</option>
         </select>
       </form >
       )
