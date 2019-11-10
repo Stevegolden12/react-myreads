4c212d700d2a48cf74bd5c0cf7df62987e35343b
Steve Golden <sgolden2525@outlook.com>
2019-11-10 05:25:03 -0600

    Fix book shelf optoin selected
    Added fix to show in shelf book drop down which option is selected.

--- src/components/ShelfTransferMenu.js
+++ src/components/ShelfTransferMenu.js
@@ -11,13 +11,15 @@ class ShelfTransferMenu extends Component {
 
   render() {  
     let shelf = this.props.isShelf;
+    console.log(shelf)
+    console.log()
      return (
       < form >
          <select className="main-page__select" size="5" visiblity={this.props.isSelectVisible === true ? 'visible' : 'hidden'} defaultValue={this.props.shelf} onChange={(event) => { this.props.changeBookShelf(event, this.props.book.id); this.props.toggleOptionNoneSelect() }} >
           <option disabled>Move to...</option>
-           <option className="main-page__option" value="currentlyReading">Currently Reading</option>
-           <option className="main-page__option" value="wantToRead">Want to Read</option>
-           <option className="main-page__option" value="read">Read</option>
+           <option className="main-page__option" value="currentlyReading" selected={shelf==='currentlyReading' ? true : false}>Currently Reading</option>
+           <option className="main-page__option" value="wantToRead" selected={shelf === 'wantToRead' ? true : false}>Want to Read</option>
+           <option className="main-page__option" value="read" selected={shelf === 'read' ? true : false}>Read</option>
            <option className="main-page__option" value="none">None</option>
         </select>
       </form >
